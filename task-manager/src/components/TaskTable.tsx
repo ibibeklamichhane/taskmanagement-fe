2c2f6673e41a0fc57bb83dd53e60844c452a33bc
ibibeklamichhane <bibeklc714@gmail.com>
2024-12-11 14:10:46 +0545

    feat: added the pagination functionality to table data

--- task-manager/src/components/TaskTable.tsx
+++ task-manager/src/components/TaskTable.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table, Button, Space } from "antd";
 import { Task } from "../interfaces/taskInterface";
 import dayjs from "dayjs";
-import { useDeleteTaskMutation } from "../api/task";
+import { useDeleteTaskMutation, fetchTasks } from "../api/task";
+import { useQuery } from "react-query";
 
 interface TaskTableProps {
   tasks: Task[];
   onEdit: (task: Task) => void;
 }
 
-const TaskTable: React.FC<TaskTableProps> = ({ tasks, onEdit }) => {
+const TaskTable: React.FC<TaskTableProps> = ({ onEdit }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+
+  const { data } = useQuery(
+    ["tasks", currentPage, pageSize],
+    () => fetchTasks(currentPage, pageSize),
+    { keepPreviousData: true }
+  );
   const deleteMutation = useDeleteTaskMutation();
 
+  const handleTableChange = (pagination: any) => {
+    setCurrentPage(pagination.current);
+    setPageSize(pagination.pageSize);
+  };
   return (
     <Table
-      dataSource={tasks || []}
+      dataSource={data?.tasks || []}
       rowKey="_id"
+      pagination={{
+        current: currentPage,
+        pageSize: pageSize,
+        total: data?.totalCount || 0,
+        showSizeChanger: true,
+      }}
+      onChange={handleTableChange}
       columns={[
         {
           title: "Title",
